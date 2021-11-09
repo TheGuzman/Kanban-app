fedc2718d92ed317b2a80ca786d7538d492a6f30
Diego Guzmán Medrano <guzmanmedrano.diego@gmail.com>
2021-11-09 17:25:48 +0100

    componentes del tablero añadidos

---
+++ kanban-app/src/components/new-task-generator/new-task-generator.jsx
@@ -0,0 +1,16 @@
+
+
+function NewTaskGenerator(){
+
+    return(
+        <div>
+            <input type='text' placeholder='enter a task'></input>
+            <div>
+                <button>Add</button>
+                <button>Cancel</button>
+            </div>
+        </div>
+    )
+}
+
+export default NewTaskGenerator
