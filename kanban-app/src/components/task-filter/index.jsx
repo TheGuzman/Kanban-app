332ba2fda91083d7524769fd2ade8a16b6565e69
Pedri <pedritociudad@hotmail.es>
2021-11-11 17:46:25 +0100

    Header, subheader y un cambio en el Vh para que no haya scroll

---
+++ kanban-app/src/components/task-filter/index.jsx
@@ -0,0 +1,10 @@
+import './style.css'
+
+function TaskFilter(){
+
+    return(
+            <input className="input__container" type="text" placeholder="&#128269; Filter cards" />
+
+    )
+}
+export default TaskFilter
