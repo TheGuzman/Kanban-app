db50f292003b9b6e549527813e94142eec7fc3ec
Diego Guzmán Medrano <guzmanmedrano.diego@gmail.com>
2021-11-12 18:09:14 +0100

    cambio de mapeo de columnas a componentes columna

--- kanban-app/src/components/task-board/task-board.jsx
+++ kanban-app/src/components/task-board/task-board.jsx
@@ -1,13 +1,18 @@
 import React from "react"
+import './style.css'
 import Column from "../column/column"
-
+import DoneColumn from "../done-column/column"
 
 
 function TaskBoard() {
 
     return (
         <React.Fragment>
-            <Column></Column>
+            <div className='columns__container'>
+            <Column title='To do'></Column>
+            <Column title='In progress'></Column>
+            <DoneColumn title='Done'></DoneColumn>
+            </div>
         </React.Fragment>
     )
 }
